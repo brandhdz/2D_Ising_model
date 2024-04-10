c90e465a92aa6f4a82a5087164d71c1b60d964ae
Hodek <hodek.mealstrom@gmail.com>
2024-04-09 18:00:53 -0600

    Added parameters module

---
+++ src/parameters.f90
@@ -0,0 +1,29 @@
+module parameters
+
+    use iso_fortran_env, only : dp => real64, i4 => int32
+    implicit none
+  
+    integer(i4) :: L !Size of the lattice
+    character(100) :: input_file, start
+  
+    namelist /input_parameters/ L
+  
+  contains
+  
+    subroutine read_input
+  
+      integer(i4) :: unit
+  
+      read(*,*) input_file
+  
+      open(newunit = unit, file = input_file)
+      read(unit, nml = input_parameters)
+      close(unit)
+  
+      if( L <= 0 ) error stop "L must be > 0"
+      write(*, nml = input_parameters)
+  
+    end subroutine read_input
+      
+  end module parameters
+  
