b8e4fce9106691c040f5fcdced405f87b8fd8b71
Hodek <hodek.mealstrom@gmail.com>
2024-04-09 18:04:52 -0600

    Added more parameters

--- src/parameters.f90
+++ src/parameters.f90
@@ -3,10 +3,11 @@ module parameters
     use iso_fortran_env, only : dp => real64, i4 => int32
     implicit none
   
-    integer(i4) :: L !Size of the lattice
+    integer(i4) :: L, N_thermalization, N_measurements, N_skip
+    real(dp) :: dt, epsilon, lambda
     character(100) :: input_file, start
   
-    namelist /input_parameters/ L
+    namelist /input_parameters/ N_thermalization, N_measurements, N_skip, L, dt, epsilon,lambda, start
   
   contains
   
@@ -21,6 +22,12 @@ module parameters
       close(unit)
   
       if( L <= 0 ) error stop "L must be > 0"
+      if( dt <= 0 ) error stop "dt must be > 0"
+      if( epsilon <= 0 ) error stop "epsilon must be > 0"
+      if( N_thermalization <= 0 ) error stop "N_thermalization must be > 0"
+      if( N_measurements <= 0 ) error stop "N_measurements must be > 0"
+      if( N_skip <= 0 ) error stop "N_skip must be > 0"
+  
       write(*, nml = input_parameters)
   
     end subroutine read_input
