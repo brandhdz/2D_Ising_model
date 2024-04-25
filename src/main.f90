2d00dd29e77a96baf2a983db4f2d54d5253e87ee
Brandon Hernández <abrandhdz@gmail.com>
2024-04-25 15:46:06 -0600

    Energy module created

--- src/main.f90
+++ src/main.f90
@@ -1,8 +1,14 @@
 program main
 
+  use iso_fortran_env, only : dp => real64
   use periodic_boundary_conditions
+  use energy
   implicit none
 
-  call set_boundary_conditions(5)
+  real(dp), dimension(2,2)  :: x
+  
+  call set_boundary_conditions(2)
+  x = reshape([1, -1, 1, -1], [2, 2])
+  call hamiltonian(x, 2, 1.0_dp)
   
 end program main
