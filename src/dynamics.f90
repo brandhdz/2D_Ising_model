075daf5afca0e5d5ea4dca9df31404128dce95a0
Hodek <hodek.mealstrom@gmail.com>
2024-05-06 23:21:20 -0600

    Added the call for the delta_hamiltonian rubroutine from the energy module

--- src/dynamics.f90
+++ src/dynamics.f90
@@ -2,19 +2,22 @@ module dynamics
 
     use iso_fortran_env, only : dp => real64, i4 => int32
     use local_update_algorithms
+    use energy
     implicit none
   
   contains
   subroutine sweep(x)
-    integer(i4) :: i,j,xnew
+    integer(i4) :: i, j, xnew
     real(dp) :: dh !Energy diferrence
+
     integer(i4), intent(inout), dimension(:,:)  :: x !Lattice
-    dh = 0.01_dp
+    
 
     do i = 1, size(x(1, :))
         do j = 1, size(x(1, :))
-            !call random_number(r)
+
             xnew = -x(i,1)
+            call delta_hamiltonian(x, i, j)
             call metropolis(x(i,j), xnew, dh)
         end do
     end do
