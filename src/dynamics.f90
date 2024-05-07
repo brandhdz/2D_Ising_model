b2ae41c3e8ad61d6d09f7ec70575159c62ba8fea
Hodek <hodek.mealstrom@gmail.com>
2024-05-06 22:53:32 -0600

    Added dynamics module with the sweep subroutine

---
+++ src/dynamics.f90
@@ -0,0 +1,27 @@
+module dynamics
+
+    use iso_fortran_env, only : dp => real64, i4 => int32
+    use local_update_algorithms
+    implicit none
+  
+  contains
+  
+    subroutine sweep(x)
+  
+      integer(i4) :: i,j,xnew
+      real(dp) :: dh !Energy diferrence
+      integer(i4), intent(inout), dimension(:,:)  :: x !Lattice
+      
+      do i = 1, size(x(1, :))
+        do j = 1, size(x(1, :))
+            !call random_number(r)
+            xnew = -x(i,1)
+            call metropolis(x(i,j), xnew, dh)
+         
+      end do
+    end do
+      
+    end subroutine sweep
+
+    
+  end module dynamics
