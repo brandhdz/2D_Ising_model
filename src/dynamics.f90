c072116362246281469b76512cb299340547024d
Hodek <hodek.mealstrom@gmail.com>
2024-05-06 23:05:02 -0600

    Corrections to the dynamics algorithm

--- src/dynamics.f90
+++ src/dynamics.f90
@@ -5,20 +5,18 @@ module dynamics
     implicit none
   
   contains
-  
-    subroutine sweep(x)
-  
-      integer(i4) :: i,j,xnew
-      real(dp) :: dh !Energy diferrence
-      integer(i4), intent(inout), dimension(:,:)  :: x !Lattice
-      
-      do i = 1, size(x(1, :))
+  subroutine sweep(x)
+    integer(i4) :: i,j,xnew
+    real(dp) :: dh !Energy diferrence
+    integer(i4), intent(inout), dimension(:,:)  :: x !Lattice
+    dh = 0.01_dp
+
+    do i = 1, size(x(1, :))
         do j = 1, size(x(1, :))
             !call random_number(r)
             xnew = -x(i,1)
             call metropolis(x(i,j), xnew, dh)
-         
-      end do
+        end do
     end do
       
     end subroutine sweep
