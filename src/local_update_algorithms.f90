bd228a04ffd7610e41798f5d6d8634e4e8851fb0
Hodek <hodek.mealstrom@gmail.com>
2024-05-06 22:35:05 -0600

    Changes from real to integer for the array entries for each algorithm

--- src/local_update_algorithms.f90
+++ src/local_update_algorithms.f90
@@ -1,15 +1,15 @@
 module local_update_algorithms
 
-    use iso_fortran_env, only : dp => real64
+    use iso_fortran_env, only : dp => real64, i4 => int32
     implicit none
   
   contains
   
     subroutine metropolis(x0, xnew, dh)
       real(dp) :: r, p
-      real(dp), intent(inout) :: x0
-      real(dp), intent(in) :: xnew, dh
-      
+      integer(i4), intent(inout) :: x0, xnew
+      real(dp), intent(in) :: dh
+    
       call random_number(r)
       p = MIN(1.0_dp, EXP(-dh))
   
@@ -22,8 +22,8 @@ module local_update_algorithms
     subroutine glauber(x0, xnew, dh)
   
       real(dp) :: r, p
-      real(dp), intent(inout) :: x0
-      real(dp), intent(in) :: xnew, dh
+      integer(i4), intent(inout) :: x0, xnew
+      real(dp), intent(in) :: dh
   
       call random_number(r)
       p = 1/(EXP(dh)+1.0_dp)
