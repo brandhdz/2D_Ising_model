1d59ef7f4670012fbca74f29a4a3e125547169ac
Brandon Hernández <abrandhdz@gmail.com>
2024-05-07 17:55:05 -0600

    Sweep module tested

--- src/dynamics.f90
+++ src/dynamics.f90
@@ -2,6 +2,8 @@ module dynamics
 
     use iso_fortran_env, only : dp => real64, i4 => int32
     use local_update_algorithms
+    use energy
+    
     implicit none
   
   contains
@@ -9,13 +11,13 @@ module dynamics
     integer(i4) :: i,j,xnew
     real(dp) :: dh !Energy diferrence
     integer(i4), intent(inout), dimension(:,:)  :: x !Lattice
-    dh = 0.01_dp
 
     do i = 1, size(x(1, :))
         do j = 1, size(x(1, :))
             !call random_number(r)
-            xnew = -x(i,1)
-            call metropolis(x(i,j), xnew, dh)
+           xnew = -x(i,1)
+           call delta_hamiltonian(x, i, j)
+           call metropolis(x(i,j), xnew, dh)
         end do
     end do
       
--- src/main.f90
+++ src/main.f90
@@ -6,22 +6,18 @@ program main
   use periodic_boundary_conditions
   use energy
   use local_update_algorithms
-
-  implicit none
-  real(dp) :: xn, dh
-  DH =  0.01_dp
+  use dynamics
   
+  implicit none
   
   call read_input
   call set_memory_lattice(L)
-  call setInitialConfig(x, start )
+  call setInitialConfig(x, start)
   call set_boundary_conditions(L)
-  call hamiltonian(x, L)
-  
-  print*, x(1,1)
-  xn = -x(1,1)
-  !call metropolis(x(1,1), xn, DH)
-  !call glauber(x(1,1), xn, DH)
-  print*, x(1,1)
+  print*, "Original"
+  print*, x
+  print*, "Sweep"
+  call sweep(x)
+  print*, x
  
 end program main
